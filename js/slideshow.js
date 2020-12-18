a2361ebe0fd2da07a1bf695abf165f65fc2c29ea
ThanhHai76 <haint62@wru.vn>
2020-12-18 10:55:25 +0700

    fix slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,6 @@
 let currentSlideIndex = 0;
-let slideArray = [];// Mang luu cac slide
+let slideArray = []; // Mang luu cac slide
+let dotArray = [];
 
 //Ham tao ra doi tuong slide
 function Slide(title1, title2, image, content, button1, button2) {
@@ -13,6 +14,12 @@ function Slide(title1, title2, image, content, button1, button2) {
   this.class = "slider-dark ";
 
   slideArray.push(this);
+}
+
+function Dot(currentSlide) {
+  this.currentSlide = currentSlide;
+  this.class = "dot";
+  dotArray.push(this);
 }
 
 //Them cac doi tuong slide
@@ -40,15 +47,38 @@ let slide3 = new Slide(
   "Purchase now",
   "More details"
 );
+let slide4 = new Slide(
+  "Pricing",
+  " Tables 4",
+  "./images/icon-slider.png",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ullam minima iure aliquam sed. Delectus quos obcaecati id similique! Numquam commodi praesentium neque est ex cumque voluptate voluptatum aliquam doloremque?",
+  "Purchase now",
+  "More details"
+);
+let slide5 = new Slide(
+  "Pricing",
+  " Tables 5",
+  "./images/icon-slider.png",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ullam minima iure aliquam sed. Delectus quos obcaecati id similique! Numquam commodi praesentium neque est ex cumque voluptate voluptatum aliquam doloremque?",
+  "Purchase now",
+  "More details"
+);
+
+let dot1 = new Dot(0);
+let dot2 = new Dot(1);
+let dot3 = new Dot(2);
+let dot4 = new Dot(3);
+let dot5 = new Dot(4);
 
 // Tu mang slide da tao, dua vao HTML
 function buildSlider() {
   let myHTML;
+  let myDot;
 
   for (let i = 0; i < slideArray.length; i++) {
     myHTML +=
       "<div class='" +
-      slideArray[i].class + 
+      slideArray[i].class +
       "'>" +
       "<img class='image' src='" +
       slideArray[i].image +
@@ -75,23 +105,38 @@ function buildSlider() {
       "</a>" +
       "</div>" +
       "</div>";
+
+    myDot +=
+      "<span class='" +
+      dotArray[i].class +
+      "' name='" +
+      dotArray[i].currentSlide +
+      "'></span>";
   }
 
   // Dua HTML vua tao vao class slideshow
-  document.getElementsByClassName("slideshow")[0].innerHTML = myHTML;
+  document.querySelector(".slideshow").innerHTML = myHTML;
+  document.querySelector(".currentSlide").innerHTML = myDot;
   // Hien thị slide dau tien
-  document.getElementsByClassName("slider-dark")[currentSlideIndex].style.left = 0;
+  document.querySelector(".slider-dark").style.left = 0;
 }
 
 // Goi ham thuc thi
 buildSlider();
 
-let dots = document.getElementsByClassName("dot");
-dots[currentSlideIndex].className += " active";
+let slider = document.querySelectorAll(".slider-dark");
+let prevSlide = document.querySelector(".prev");
+let nextSlide = document.querySelector(".next");
 
-function prevSlide() {
-  let nextSlideIndex;// Tim slide truoc do
-  if (currentSlideIndex === 0) {// Neu slideIndex = 0 thi ve slide cuoi
+let dots = document.querySelectorAll(".dot");
+dots[currentSlideIndex].classList.add("active");
+
+prevSlide.addEventListener("click", prevSlide_func);
+
+function prevSlide_func() {
+  let nextSlideIndex; // Tim slide truoc do
+  if (currentSlideIndex === 0) {
+    // Neu slideIndex = 0 thi ve slide cuoi
     nextSlideIndex = slideArray.length - 1;
   } else {
     // Neu khong thi giam index di 1
@@ -103,22 +148,20 @@ function prevSlide() {
   dots[nextSlideIndex].className += " active"; // Show dot current, them class active
 
   // An slide hien tai, hien slide "currentSlideIndex"
-  document.getElementsByClassName("slider-dark")[nextSlideIndex].style.left = "-100%";
-  document.getElementsByClassName("slider-dark")[currentSlideIndex].style.left = 0;
+  slider[nextSlideIndex].style.left = "-100%";
+  slider[currentSlideIndex].style.left = 0;
 
   // Them class de chuyen slide co animation
-  document
-    .getElementsByClassName("slider-dark")[nextSlideIndex]
-    .setAttribute("class", "slider-dark slideInLeft");
-  document
-    .getElementsByClassName("slider-dark")[currentSlideIndex]
-    .setAttribute("class", "slider-dark slideOutRight");
+  slider[nextSlideIndex].setAttribute("class", "slider-dark slideInLeft");
+  slider[currentSlideIndex].setAttribute("class", "slider-dark slideOutRight");
 
   // Cap nhat gia tri slide hien tai
   currentSlideIndex = nextSlideIndex;
 }
 
-function nextSlide() {
+nextSlide.addEventListener("click", nextSlide_func);
+
+function nextSlide_func() {
   let nextSlideIndex;
   if (currentSlideIndex === slideArray.length - 1) {
     nextSlideIndex = 0;
@@ -129,35 +172,38 @@ function nextSlide() {
     dots[i].className = dots[i].className.replace(" active", ""); // An cac dot con lai
   }
   dots[nextSlideIndex].className += " active"; // Show dot current, them class active
-  
-  document.getElementsByClassName("slider-dark")[nextSlideIndex].style.left = "100%";
-  document.getElementsByClassName("slider-dark")[currentSlideIndex].style.left = 0;
 
-  document
-    .getElementsByClassName("slider-dark")[nextSlideIndex]
-    .setAttribute("class", "slider-dark slideInRight");
-  document
-    .getElementsByClassName("slider-dark")[currentSlideIndex]
-    .setAttribute("class", "slider-dark slideOutLeft");
+  slider[nextSlideIndex].style.left = "100%";
+  slider[currentSlideIndex].style.left = 0;
+
+  slider[nextSlideIndex].setAttribute("class", "slider-dark slideInRight");
+  slider[currentSlideIndex].setAttribute("class", "slider-dark slideOutLeft");
 
   currentSlideIndex = nextSlideIndex;
 }
 
+dots.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    count = Number(elem.getAttribute("name"));
+    showSlides(count);
+  });
+});
+
 //Show slide khi nhan vao dot
 function showSlides(nextSlideIndex) {
-  if(currentSlideIndex === 2 && nextSlideIndex === 0){
-    nextSlide()
-  }
-  if(currentSlideIndex === 0 && nextSlideIndex === 2){
-    prevSlide();
-  }
-  if (currentSlideIndex < nextSlideIndex){
-    for(let i=currentSlideIndex; i<nextSlideIndex; i++){
-      nextSlide();
+  if (currentSlideIndex == 4 && nextSlideIndex == 0) {
+    nextSlide_func();
+  } 
+  if (currentSlideIndex == 0 && nextSlideIndex == 4) {
+    prevSlide_func();
+  } 
+  if (currentSlideIndex < nextSlideIndex) {
+    for (let i = currentSlideIndex; i < nextSlideIndex; i++) {
+      nextSlide_func();
     }
   } else {
-    for(let i=nextSlideIndex; i<currentSlideIndex; i++){
-      prevSlide();
+    for (let i = currentSlideIndex; i > nextSlideIndex ; i--) {
+      prevSlide_func();
     }
   }
 }
